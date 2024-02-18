08ac9443e14ff435a99a1e5cc7bd9634be1b60e1
taniasumona15 <taniasumona53989@gmail.com>
2024-02-18 17:38:17 +0600

    10th commit

--- js/alpha.js
+++ js/alpha.js
@@ -52,6 +52,11 @@ function setBackgroundColorById(elementId){
        element.classList.add('bg-orange-400');
 
 }
+function removeBackgroundColorById(elementId){
+    const element=document.getElementById(elementId);
+       element.classList.remove('bg-orange-400');
+
+}
 
 
 function hideElementById(elementId){
@@ -66,3 +71,115 @@ function removeElementById(elementId){
 }
 
 
+
+
+// function handleKeyboardButtonPress(event){
+// const playerPressed=event.key;
+
+
+// //get the expected to press 
+// const currentAlphabetElement=document.getElementById('current-alphabet');
+// const currentAlphabet= currentAlphabetElement.innerText;
+// const expectedAlphabet= currentAlphabet.toLowerCase();
+// console.log(playerPressed,expectedAlphabet);
+
+// //check if matched or not
+// if(playerPressed===expectedAlphabet){
+//    // console.log('matched');
+
+// //update score
+// //1. get the current score
+// const currentScoreElement=document.getElementById('current-score');
+// const currentScoreText=currentScoreElement.innerText;
+// const currentScore=parseInt(currentScoreText);
+
+// //2 increase the score by 1
+
+// const newScore=currentScore+1;
+
+// //3 show the updated score
+
+// currentScoreElement.innerText=newScore;
+
+
+
+
+
+
+
+
+
+
+
+//    removeBackgroundColorById(expectedAlphabet);
+//    continueGame();
+    
+// }
+
+// else{
+// //1 get the current life
+// const currentLifeElement=document.getElementById('life-score');
+// const currentLifeText=currentLifeElement.innerText;
+
+// const currentLife=parseInt(currentLifeText);
+
+
+// //2 reduce the life count
+// const newLifeCount=currentLife-1;
+
+// //3 display the current life
+
+// currentLifeElement.innerText=newLifeCount;
+// }
+// }
+// // capture keyboard key press
+// document.addEventListener('keyup', handleKeyboardButtonPress);
+
+
+
+function getTextElementValueById(elementId){
+const element= document.getElementById(elementId);
+const elementValueText=element.innerText;
+const value= parseInt(elementValueText);
+return value;
+
+}
+
+function setTextElementValueById(elementId,value){
+const element= document.getElementById(elementId);
+element.innerText=value;
+}
+
+
+function handleKeyboardButtonPress(event){
+    const playerPressed=event.key;
+    // console.log(playerPressed)
+    const currentAlphabetElement=document.getElementById('current-alphabet');
+const currentAlphabet= currentAlphabetElement.innerText;
+const expectedAlphabet= currentAlphabet.toLowerCase();
+
+
+
+
+    if(playerPressed===expectedAlphabet){
+        const currentScore=getTextElementValueById('current-score');
+        console.log(currentScore)
+        const newScore=currentScore+1;
+         setTextElementValueById('current-score', newScore);
+         removeBackgroundColorById(expectedAlphabet);
+            continueGame();
+             
+    }
+
+    else{
+        const currentLife=getTextElementValueById('life-score');
+        const updatedLife=currentLife-1;
+        setTextElementValueById('life-score', updatedLife);
+
+        if(updatedLife===0){
+            
+        }
+        }
+}
+
+document.addEventListener('keyup', handleKeyboardButtonPress);
